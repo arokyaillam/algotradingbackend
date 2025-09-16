37ce7b6d1597ca2e44e6595aaa60f61fd1575bd8
arokyaillam <arokyaillam@gmail.com>
2025-09-16 13:55:05 +0530

    schema wsa changed

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255 }).notNull(),
+hashedPassword: text('hashed_password').notNull(),
 });
+
